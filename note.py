da3d6b595089b01f69bf8270993f9a6906deb1f2
Lucie Cupcakes <lucie_linux@protonmail.com>
2021-08-12 15:02:22 +0000

    fast_commit at 20210812_150222

--- note.py
+++ note.py
@@ -1,13 +1,23 @@
 from uuid import uuid4
 from datetime import datetime
-
+from pepino import PepinoDB
+from json import dumps as json_dumps
+from json import loads as json_loads
 class Note:
     def __init__(self, title : str, contents : str):
-        self.id = uuid4()
+        self.id = str(uuid4())
         self.title = title
         self.contents = contents
         self.creation_time = datetime.utcnow()
         self.last_modified = self.creation_time
+    @staticmethod
+    def from_json(json_data : str):
+        data = json_loads(json_data)
+        note = Note(data.title, data.contents)
+        note.id = data.id
+        note.creation_time = data.creation_time
+        note.last_modified = data.last_modified
+        return note
     def modify(self, title : str, contents : str):
         self.title = title
         self.contents = contents
@@ -16,4 +26,10 @@ class Note:
         res = f"title: {self.title}\nCreation time: {self.creation_time}"
         f"\nModification time: {self.last_modified}\n{self.contents}"
         return res
-
+    def save(self, db_handler : PepinoDB):
+        note_bytes = json_dumps(self).encode("utf-8")
+        db_handler.save_entry(self.id, note_bytes)
+    @staticmethod
+    def load(note_id : str, db_handler : PepinoDB):
+        note_json = db_handler.get_entry(note_id).decode("utf-8")
+        return Note.from_json(note_json)
--- program.py
+++ program.py
@@ -1,7 +1,42 @@
 from pepino import PepinoDB
+from note import Note
+from notelist import NoteList
 class Program:
     def __init__(self):
         self.__db_handle = PepinoDB("http://localhost:50200", "NotesPy", "caipiroska")
+        try:
+            self.__note_list = NoteList.load(self.__db_handle)
+        except:
+            self.__note_list = NoteList()
     def run(self):
-        print("Run program")
+        cmd = input("Notes>")
+        if cmd.startswith("new"):
+            self.__new_command()
+        elif cmd.startswith("list"):
+            self.__list_command()
+        elif cmd.startswith("mod"):
+            self.__modify_command()
+        elif cmd.startswith("del"):
+            self.__delete_command()
+        elif cmd.startswith("print"):
+            self.__print_command()
+        elif cmd.startswith("help"):
+            self.__help_command()
+        else:
+            print("Command not found")
+    def __read_until_finish(self) -> str:
+        result = ""
+        while not (line := input()).rstrip().startswith("@finish@"):
+            result = f"{result}\n{line}"
+    def __help_command(self):
 
+    def __new_command(self):
+        try:
+            title = input("title: ")
+            contents = self.__read_until_finish()
+            note = Note(title, contents)
+            note.save(self.__db_handle)
+            self.__note_list.put(note.id, note.title)
+            self.__note_list.save(self.__db_handle)
+        except Exception as e:
+            print(e)
