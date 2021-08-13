3d7bb273df2bcd4500837dd58e6f1a88ada09bcd
Lucie Cupcakes <lucie_linux@protonmail.com>
2021-08-13 04:29:09 +0000

    fast_commit at 20210813_042909

--- note.py
+++ note.py
@@ -1,3 +1,4 @@
+from typing import Dict
 from uuid import uuid4
 from datetime import datetime
 from pepino import PepinoDB
@@ -12,23 +13,36 @@ class Note:
         self.last_modified = self.creation_time
     @staticmethod
     def from_json(json_data : str):
+        dtfmt = "%Y-%m-%dT%H:%M:%S.%f"
         data = json_loads(json_data)
-        note = Note(data.title, data.contents)
-        note.id = data.id
-        note.creation_time = data.creation_time
-        note.last_modified = data.last_modified
+        note = Note(data["title"], data["contents"])
+        note.id = data["id"]
+        note.creation_time = datetime.strptime(data["creation_time"], dtfmt)
+        note.last_modified = datetime.strptime(data["last_modified"], dtfmt)
         return note
+    def to_json(self) -> str:
+        dtfmt = "%Y-%m-%dT%H:%M:%S.%f"
+        data : Dict[str, str] = {}
+        data["id"] = self.id
+        data["title"] = self.title
+        data["contents"] = self.contents
+        data["creation_time"] = self.creation_time.strftime(dtfmt)
+        data["last_modified"] = self.last_modified.strftime(dtfmt)
+        return json_dumps(data)
     def modify(self, title : str, contents : str):
         self.title = title
         self.contents = contents
         self.last_modified = datetime.utcnow()
     def to_string(self) -> str:
-        res = f"title: {self.title}\nCreation time: {self.creation_time}"
-        f"\nModification time: {self.last_modified}\n{self.contents}"
-        return res
+        dtfmt = "%Y-%m-%dT%H:%M:%S.%f"
+        creation_time_str = self.creation_time.strftime(dtfmt)
+        last_modified_str = self.last_modified.strftime(dtfmt)
+        s = f"title: {self.title}\nCreation time: {creation_time_str}"
+        s = f"{s}\nModification time: {last_modified_str}\n{self.contents}"
+        return s
     def save(self, db_handler : PepinoDB):
-        note_bytes = json_dumps(self).encode("utf-8")
-        db_handler.save_entry(self.id, note_bytes)
+        json_bytes = self.to_json().encode("utf-8")
+        db_handler.save_entry(self.id, json_bytes)
     @staticmethod
     def load(note_id : str, db_handler : PepinoDB):
         note_json = db_handler.get_entry(note_id).decode("utf-8")
--- program.py
+++ program.py
@@ -9,34 +9,77 @@ class Program:
         except:
             self.__note_list = NoteList()
     def run(self):
-        cmd = input("Notes>")
-        if cmd.startswith("new"):
-            self.__new_command()
-        elif cmd.startswith("list"):
-            self.__list_command()
-        elif cmd.startswith("mod"):
-            self.__modify_command()
-        elif cmd.startswith("del"):
-            self.__delete_command()
-        elif cmd.startswith("print"):
-            self.__print_command()
-        elif cmd.startswith("help"):
-            self.__help_command()
-        else:
-            print("Command not found")
+        print("Welcome to the Notes program!\n"
+              "TIP: type help for the command list")
+        while True:
+            cmd = input("Notes>").lstrip().rstrip()
+            self.__cmd = cmd
+            if cmd.startswith("new"):
+                self.__new_command()
+            elif cmd.startswith("list"):
+                self.__list_command()
+            elif cmd.startswith("mod"):
+                self.__modify_command()
+            elif cmd.startswith("del"):
+                self.__delete_command()
+            elif cmd.startswith("print"):
+                self.__print_command()
+            elif cmd.startswith("help"):
+                self.__help_command()
+            elif cmd.startswith("exit"):
+                break
+            else:
+                print("Command not found")
     def __read_until_finish(self) -> str:
+        print("TIP: type @finish@ when you finish.")
         result = ""
         while not (line := input()).rstrip().startswith("@finish@"):
             result = f"{result}\n{line}"
+        return result
     def __help_command(self):
-
+        print("Commands:\n"
+              "new-\tCreate a Note\n"
+              "del-\tDelete a Note\n"
+              "mod-\tModify a Note\n"
+              "list-\tList Notes\n"
+              "print-\tPrint a Note to the screen\n"
+              "exit-\tLeave the program.")
     def __new_command(self):
         try:
             title = input("title: ")
             contents = self.__read_until_finish()
             note = Note(title, contents)
+            print(note.to_string())
+            print(note.to_json())
             note.save(self.__db_handle)
             self.__note_list.put(note.id, note.title)
             self.__note_list.save(self.__db_handle)
         except Exception as e:
             print(e)
+    def __list_command(self):
+        try:
+            if len(self.__note_list.value) < 1:
+                print("There are not saved notes.")
+                return
+            for note_id in self.__note_list.value:
+                note_title = self.__note_list.value[note_id]
+                print(f"{note_id}\t{note_title}")
+        except Exception as e:
+            print(e)
+    def __print_command(self):
+        try:
+            cmd_arr = self.__cmd.split(" ")
+            if len(cmd_arr) < 2:
+                print("usage: print <id>")
+                return
+            note_id = cmd_arr[1]
+            note = Note.load(note_id, self.__db_handle)
+            print(note.to_string())
+        except Exception as e:
+            print(e)
+if __name__ == "__main__":
+    try:
+        program = Program()
+        program.run()
+    except Exception as e:
+        print(e)
